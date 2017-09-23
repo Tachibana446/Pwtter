54cd7f616befba6e6e9ea2a21b5ad0d5bba43990
Tachibana446 <stinger33z@gmail.com>
2017-09-24 06:03:04 +0900

    [add]Twitterのログインまでできるようにした

---
+++ Pwtter/AuthWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pwtter
+{
+    /// <summary>
+    /// AuthWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        string url;
+        public string pin = "";
+        public bool isOk = false;
+        public bool isClosed = false;
+
+        public AuthWindow(string url)
+        {
+            this.url = url;
+            InitializeComponent();
+            urlBox.Text = url;
+        }
+
+        private void AccessButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(url);
+            ((Button)sender).IsEnabled = false;
+            okButton.IsEnabled = true;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            pin = pinBox.Text.Trim();
+            if (pin == "")
+            {
+                MessageBox.Show("PINを入力してください。");
+                return;
+            }
+            isOk = true;
+            isClosed = true;
+            Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            isClosed = true;
+        }
+    }
+}
--- Pwtter/MainWindow.xaml.cs
+++ Pwtter/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace Pwtter
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Instance { get; private set; }
+
+        public CoreTweet.Tokens TwitterTokens { get; set; } = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+        }
+
+
+        /// <summary>
+        /// メニューアイテムの認証→Twitterをクリックした時の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AuthTwitterMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var session = CoreTweet.OAuth.Authorize(Properties.Resources.TwConsumerKey, Properties.Resources.TwConsumerSecret);
+                var authWindow = new AuthWindow(session.AuthorizeUri.ToString());
+                authWindow.ShowDialog();
+                if (authWindow.isOk)
+                {
+                    TwitterTokens = CoreTweet.OAuth.GetTokens(session, authWindow.pin);
+                    // DEBUG
+                    var stackPanel = new StackPanel();
+                    foreach (var s in TwitterTokens.Statuses.HomeTimeline())
+                    {
+                        stackPanel.Children.Add(new TextBlock() { Text = s.Text });
+                    }
+                    tabsGrid.Children.Add(stackPanel);
+                }
+            }
+            catch (CoreTweet.TwitterException ex)
+            {
+                MessageBox.Show(ex.Message, "エラー");
+                return;
+            }
+        }
+
+        /// <summary>
+        /// メニューアイテムの認証→Pawooをクリックした時の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AuthPawooMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
