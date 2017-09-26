efd9424990d604e82c0b007a818d32c31651778c
Tachibana446 <stinger33z@gmail.com>
2017-09-26 09:38:13 +0900

    [add]投稿できるようになった

--- Pwtter/TweetBox.xaml.cs
+++ Pwtter/TweetBox.xaml.cs
@@ -24,5 +24,79 @@ namespace Pwtter
         {
             InitializeComponent();
         }
+
+        private async void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)twitterCheckBox.IsChecked && MainWindow.Instance.TwitterTokens == null)
+            {
+                MessageBox.Show("まずはTwitterのアカウントを認証してください。");
+                return;
+            }
+            if ((bool)pawooCheckBox.IsChecked && MainWindow.Instance.PawooClient == null)
+            {
+                MessageBox.Show("まずはPawooのアカウントを認証してください。");
+                return;
+            }
+
+            if (!(bool)twitterCheckBox.IsChecked && !(bool)pawooCheckBox.IsChecked)
+            {
+                MessageBox.Show("TwitterかPawooのうち、投稿する方どちらかあるいは両方にチェックを入れてください");
+                return;
+            }
+            var text = messageTextBox.Text.Trim();
+            if (text == "")
+            {
+                MessageBox.Show("投稿する文章が空です");
+                return;
+            }
+
+            if ((bool)twitterCheckBox.IsChecked && !(bool)canOverLengthText.IsChecked && text.Length > 140)
+            {
+                MessageBox.Show("Twitterの上限である140字を超えています。");
+                return;
+            }
+            if (text.Length > 500)
+            {
+                MessageBox.Show("Pawooの上限である500字を超えています。");
+                return;
+            }
+            submitButton.IsEnabled = false;
+            messageTextBox.Text = "";
+
+            // Twitter
+            try
+            {
+                if ((bool)twitterCheckBox.IsChecked)
+                {
+                    int c = 0;
+                    while (c < text.Length)
+                    {
+                        string updateText = text.Substring(c, Math.Min(140, text.Length));
+                        c += 140;
+                        MainWindow.Instance.TwitterTokens.Statuses.Update(status: updateText);
+                    }
+                }
+            }
+            catch (CoreTweet.TwitterException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // Pawoo
+            try
+            {
+                if ((bool)pawooCheckBox.IsChecked)
+                {
+                    await MainWindow.Instance.PawooClient.PostStatus(text, Mastonet.Visibility.Public);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            submitButton.IsEnabled = true;
+
+        }
     }
 }
